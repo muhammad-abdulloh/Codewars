d9b07995fdae9b20337ea197a580c865fb8c2b43
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-02-19 20:35:37 +0500

    Enumerable Magic #20 - Cascading Subsets 8kata

---
+++ Codewars/Probems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public static class Probems
+    {
+        public static IEnumerable<IEnumerable<int>> EachCons(int[] list, int n)
+        {
+            var result = new List<List<int>>();
+            for (int i = 0; i<= list.Count() - n; i++)
+            {
+                var sublist = new List<int>();  
+
+                for (int j = i; j < i + n; j++)
+                {
+                    sublist.Add(list[j]);
+                }
+                result.Add(sublist);
+            }
+
+            return result;
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var items = Probems.EachCons([1, 2, 3, 4, 5], 3);
+
+            foreach (var item in items)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.Write(item2 + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

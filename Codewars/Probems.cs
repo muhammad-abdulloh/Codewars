5cbd9af485ec4bde33fdf9b32e9923241287f1ec
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-02-25 23:27:09 +0500

    create email sender console app

--- Codewars/Probems.cs
+++ Codewars/Probems.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Codewars
 {
-    public static class Probems
+    public  class Probems : Probems2, Probems3, Probems4, Probems5
     {
         public static int[] DivisibleBy(int[] numbers, int divisor)
         {
@@ -17,6 +17,62 @@ namespace Codewars
                 if (numbers[x] % divisor == 0) result[x] = numbers[x];
             }
             return result; 
+        }
+    }
+
+    public class Probems2 : Probems3
+    {
+        public int[] DivisibleBy1(int[] numbers, int divisor)
+        {
+            var result = new int[1];
+
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                if (numbers[x] % divisor == 0) result[x] = numbers[x];
+            }
+            return result;
+        }
+    }
+
+    public interface Probems3
+    {
+        public static int[] DivisibleBy2(int[] numbers, int divisor)
+        {
+            var result = new int[1];
+
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                if (numbers[x] % divisor == 0) result[x] = numbers[x];
+            }
+            return result;
+        }
+    }
+
+    public interface Probems4
+    {
+        public static int[] DivisibleBy3(int[] numbers, int divisor)
+        {
+            var result = new int[1];
+
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                if (numbers[x] % divisor == 0) result[x] = numbers[x];
+            }
+            return result;
+        }
+    }
+
+    public interface Probems5
+    {
+        public static int[] DivisibleBy4(int[] numbers, int divisor)
+        {
+            var result = new int[1];
+
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                if (numbers[x] % divisor == 0) result[x] = numbers[x];
+            }
+            return result;
         }
     }
 }
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -1,16 +1,40 @@
-﻿namespace Codewars
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+using System.Reflection;
+
+namespace Codewars
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var items = Probems.MonkeyCount(6);
 
-            foreach (var item in items)
+            var fromAddress = new MailAddress("clevercoderr@gmail.com", "Muhammad Abdulloh");
+            var toAddress = new MailAddress("komilovmuhammadabdulloh@gmail.com", "Senga ketyapti usha mo'ni");
+            const string fromPassword = "zpnt kiyu dywx otys";
+            const string subject = "Dotnet qale ketyapti";
+            const string body = "bovottimi jigarim toychog'im nima gaplar";
+
+            var smtp = new SmtpClient
             {
-                
-                Console.WriteLine(item);
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
             }
         }
+
+
     }
 }
